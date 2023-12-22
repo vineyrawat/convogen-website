260a953b1c4cad63ed17edaaba9329b939bf4300
Vinay Rawat <vineyrawat@yahoo.com>
2023-12-22 14:16:43 +0530

    change download to coming soon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,7 +42,7 @@ export default function Home() {
               <div className="relative inline-flex  group">
                 <div className="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
                 <button className="relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900">
-                  Download
+                  Coming Soon
                 </button>
               </div>
             </div>
