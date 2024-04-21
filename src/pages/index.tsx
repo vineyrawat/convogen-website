b8bcb79659eff0f1d25f54b16a7682bc7f7c79b3
Thakur Vinay Rawat <vineyrawat@yahoo.com>
2024-04-21 13:54:01 +0530

    added web preview

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,10 @@ import Image from "next/image";
 import {
   IoIosArrowDropdown,
   IoLogoAndroid,
-  IoLogoGithub,
+  IoLogoGithub
 } from "react-icons/io";
+import { IoEarth } from "react-icons/io5";
+
 
 export default function Home() {
   return (
@@ -68,6 +70,21 @@ export default function Home() {
                       Android{" "}
                       <span className="ml-2 font-semibold text-sm text-gray-500 bg-gray-800 px-1 py-0.5 rounded">
                         APK
+                      </span>
+                    </button>
+                  </div>
+                </a>
+                <a
+                  href="https://vinaybyte.in/convogen-web-preview/"
+                  target="_blank"
+                >
+                  <div className="relative inline-flex  group">
+                    <div className="absolute transitiona-all duration-1000 opacity-50 -inset-px bg-gradient-to-r from-teal-500 via-indigo-500 to-blue-500 rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
+                    <button className="relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900">
+                      <IoEarth size={25} style={{ marginRight: "5px" }} />
+                      Web{" "}
+                      <span className="ml-2 font-semibold text-sm text-gray-500 bg-gray-800 px-1 py-0.5 rounded">
+                        Preview
                       </span>
                     </button>
                   </div>
